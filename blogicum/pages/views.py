dc8bf20fe52dc459cee7d701d0159b262a3169ed
Александр Шубенкин <sasha758516@gmail.com>
2026-05-12 01:03:46 +0300

    Исправлены импорты в pages

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
@@ -10,15 +11,12 @@ class RulesView(TemplateView):
 
 
 def page_not_found(request, exception):
-    from django.shortcuts import render
     return render(request, 'pages/404.html', status=404)
 
 
 def csrf_failure(request, reason=''):
-    from django.shortcuts import render
     return render(request, 'pages/403csrf.html', status=403)
 
 
 def server_error(request):
-    from django.shortcuts import render
     return render(request, 'pages/500.html', status=500)
